76d4796a46c3bfee11121af1f2cee12436be0898
Ansari <muhammedansari73@gmail.com>
2023-10-04 09:42:25 +0530

    added java

---
+++ JAVA/JAVA_A/ShuffleArray.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+
+        List<Integer> myList = new ArrayList<>();
+        
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(6);
+        myList.add(7);
+
+        // Shuffle
+        Collections.shuffle(myList);
+
+        //  list to an array
+        Integer[] shuffledArray = myList.toArray(new Integer[0]);
+
+        // shuffled array
+        for (Integer value : shuffledArray) {
+            System.out.print(value + " ");
+        }
+    }
+}
